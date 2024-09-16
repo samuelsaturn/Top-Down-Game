0a53e39be2bbe1a2df3ce0d46d036870b9ddce8a
Saturno <samuelmathias033@gmail.com>
2024-09-15 22:52:47 -0300

    att player sprite and movement player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,101 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private float runSpeed = 2f;
-
-    private PlayerControls playerControls;
-    private Vector2 movement;
+    public float moveSpeed = 5f;
+    public float runSpeed = 10f;  // Velocidade ao correr
+    public Animator myAnimator;
     private Rigidbody2D rb;
-    private bool isRunning;
-    private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
+    private Vector2 movement;
+    private Vector2 lastDirection;
 
     private void Awake()
     {
-        playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-        playerControls.Movement.Run.started += OnRunStarted;
-        playerControls.Movement.Run.canceled += OnRunCanceled;
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-        playerControls.Movement.Run.started -= OnRunStarted;
-        playerControls.Movement.Run.canceled -= OnRunCanceled;
     }
 
     private void Update()
     {
-        PlayerInput();
-        // Move();
+        // Captura a entrada do teclado
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Define a direção de movimento
+        movement = new Vector2(moveX, moveY).normalized;
+
+        // Atualiza a última direção de movimento
+        if (movement != Vector2.zero)
+        {
+            lastDirection = movement;
+        }
+
+        // Checa se o Shift está pressionado e ajusta a velocidade de movimento
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        // Atualiza os parâmetros de animação
+        myAnimator.SetFloat("moveX", lastDirection.x);
+        myAnimator.SetFloat("moveY", lastDirection.y);
+        myAnimator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // Atualiza a movimentação
+        MovePlayer(movement, currentSpeed);
+    }
+
+    private void MovePlayer(Vector2 direction, float speed)
+    {
+        // Move o jogador com base na direção e velocidade
+        Vector2 newPosition = rb.position + direction * speed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
     }
 
     private void FixedUpdate()
     {
-        Move();
-        // AdjustPlayerFacingDirection();
-        // FollowTarget();
-    }
-
-    // private void FollowTarget(){
-    //     Vector3 desiredPosition = target.position + offset;
-    //     Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothedSpeed);
-    //     transform.position = smoothedPosition;
-    // }
-
-    // private void LateUpdate(){
-    //     transform.position = target.position + offset;
-    // }
-
-    private void PlayerInput()
-    {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
-
-        myAnimator.SetFloat("moveX", movement.x);
-        myAnimator.SetFloat("moveY", movement.y);
-    }
-
-    private void Move()
-    {
-        Vector2 normalizedMovement = movement.normalized;
-        float currentSpeed = isRunning ? runSpeed : moveSpeed;
-        rb.MovePosition(rb.position + normalizedMovement * (currentSpeed * Time.fixedDeltaTime));
-        
-        AdjustPlayerFacingDirection();
-    }
-
-    private void OnRunStarted(InputAction.CallbackContext context)
-    {
-        isRunning = true;
-    }
-
-    private void OnRunCanceled(InputAction.CallbackContext context)
-    {
-        isRunning = false;
-    }
-
-    private void AdjustPlayerFacingDirection()
-    {
-        if (movement.x < 0)
-        {
-            mySpriteRenderer.flipX = true;
-        }
-        else if (movement.x > 0)
-        {
-            mySpriteRenderer.flipX = false;
-        }
+        // Atualize a posição do jogador
+        MovePlayer(movement, Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed);
     }
 }
